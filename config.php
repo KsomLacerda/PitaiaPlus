1ab7c6ee80eee19dcae79797add41a31500e66fa
Kleberson Lacerda <klebersonlacerda.96@gmail.com>
2022-05-24 19:03:43 -0300

    24/05 correção de bugs
    Conexão php

--- config.php
+++ config.php
@@ -1,19 +1,61 @@
 <?php
 
-    $dbHost = 'LocalHost';
-    $dbUsername = 'root';
-    $dbPassword = 'kleberson96';
+    $dbHost = 'localhost'; 
+    $dbUsername = 'root' ;
+    $dbPassword = 'Kleberson96';
     $dbName = 'LOGIN - PITAIAPLUS';
-
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-
-    if($conexao->connect_errno)
+    $port = '3306';
+    
+    $conexao = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName, $port) or die ('Não foi possivel fazer a conexão com o banco de dados!!!'); 
+/* 
+   /*  if($conexao->connect_errno)
     {
-        echo "Erro";
+        echo "Erro ao se conectar ao banco de dados";
     }
     else
     {
         echo "Conexão efetuada com sucesso";
     }
 
+
+    if(!$dbHost){
+        die ("Host incorreto!" . mysqli_connect_error());
+    }
+    else
+    {
+        echo "Host conectado com sucesso!";
+    }
+
+    print_r('<br>');
+
+    if(!$dbUsername){
+        die ("Username incorreto!" . mysqli_connect_error());
+    }
+    else
+    {
+        echo "Username conectado com sucesso!";
+    }
+
+    print_r('<br>');
+
+    if(!$dbPassword){
+        die ("Senha incorreto!" . mysqli_connect_error());
+    }
+    else
+    {
+        echo "Senha conectada com sucesso!";
+    }
+
+    print_r('<br>');
+
+    if(!$dbName){
+        die ("Nome do banco de dados incorreto!" . mysqli_connect_error());
+    }
+    else
+    {
+        echo "Nome do banco de dados conectado com sucesso!";
+    }
+
+
+    $conexao = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName) or die ('Não foi possivel fazer a conexão com o banco de dados!!!' . mysqli_connect_error()); */
 ?>    
